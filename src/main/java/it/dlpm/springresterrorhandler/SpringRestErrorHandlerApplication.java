84b3ae96bb995c6aa61dac97a0486db201eea200
damian-perez <damian-perezmedoza@digitouch.it>
2025-05-23 12:23:24 +0200

    feat: init

---
+++ src/main/java/it/dlpm/springresterrorhandler/SpringRestErrorHandlerApplication.java
@@ -0,0 +1,13 @@
+package it.dlpm.springresterrorhandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestErrorHandlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestErrorHandlerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/dlpm/springresterrorhandler/SpringRestErrorHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.dlpm.springresterrorhandler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestErrorHandlerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
